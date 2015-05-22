5f5aa04b57359a71bbdf8f3ab9c068d594394175
tuilbox <tuilbox@users.noreply.github.com>
2015-05-22 13:04:19 -0700

    working draft to Shruti

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-# The following two funcitons work together to retrieve a cacluation, if it exists
-# otherwise, it will cacluate the value. In this instance, the task is an inverted matrix.
+# The following two funcitons work together to retrieve a cacluation, if it exists.
+# Otherwise, it will calculate the value. In this instance, the task is an inverted matrix.
 # 
 
 # The first function will create a matrix, calcualte the inverse and store it for later use.
@@ -7,26 +7,30 @@
 makeCacheMatrix <- function(x = matrix()) {
        iM <- NULL
        set<- function (w) {
-              M <<- w
+              x <<- w
               iM <<- NULL
        }
+       get <- function() x
+       setIM <-function(solve) iM<<-solve
+       getIM <-function () iM
+       list (set = set, get = get,
+             setIM = setIM,
+             getIM = getIM)
 }
 
 
-# The second function will check to see if there is a stored value, and if so retrieve it
-# If not, it will compute the inverse and then store it.
+# The second function will check to see if there is a stored value, and if so retrieve it;
+# if not, it will compute the inverse and then store it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-
-
-makeMatrix <- matrix(r,c){
-      
-       get <- function() v
-       setmean <-function(mean) iM<<-mean
-       getmean <-function () iM
-       list (set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
-}
+        ## Return a matrix that is the inverse of 'x'       
+              iM <- x$getIM()
+              if(!is.null(iM)) {
+                     message ("getting cached data")
+                     return (iM)
+              }
+              data <- x$get()
+              iM<-solve(data, ...)
+              x$setIM(iM)
+              iM
+}    
